3a5c7e900fe0a9818d32abd100bf6042a12d65f1
fayozbekdew <fayozbekdev@gmail.com>
2024-09-26 14:01:46 +0500

    updated

--- js/chart3.js
+++ js/chart3.js
@@ -1,10 +1,10 @@
-var ctx2 = document.getElementById("myChart2").getContext("2d");
-const gradient2 = ctx2.createRadialGradient(200, 200, 0, 200, 200, 200); // Markazda
-gradient2.addColorStop(0, "rgba(255, 0, 0, 0.8)"); // Yuqori rang (qizil)
-gradient2.addColorStop(0.33, "rgba(255, 255, 0, 0.8)"); // O'rta yuqori rang (sariq)
-gradient2.addColorStop(0.66, "rgba(0, 255, 0, 0.8)"); // O'rta pastki rang (yashil)
-gradient2.addColorStop(0.99, "rgba(0, 0, 255, 0.5)");
-var myChart = new Chart(ctx2, {
+var ctx3 = document.getElementById("myChart3").getContext("2d");
+const gradient3 = ctx3.createRadialGradient(200, 200, 0, 200, 200, 200); // Markazda
+gradient3.addColorStop(0, "rgba(255, 0, 0, 0.8)"); // Yuqori rang (qizil)
+gradient3.addColorStop(0.33, "rgba(255, 255, 0, 0.8)"); // O'rta yuqori rang (sariq)
+gradient3.addColorStop(0.66, "rgba(0, 255, 0, 0.8)"); // O'rta pastki rang (yashil)
+gradient3.addColorStop(0.99, "rgba(0, 0, 255, 0.5)");
+var myChart = new Chart(ctx3, {
   type: "radar",
   data: {
     labels: ["A", "B", "C", "D", "E", "F"],
@@ -13,11 +13,11 @@ var myChart = new Chart(ctx2, {
         label: "My Dataset",
         data: [95, 30, 60, 50, 80, 91],
         fill: true,
-        backgroundColor: gradient2,
+        backgroundColor: gradient3,
         borderColor: "blue",
         borderWidth: 2,
-        pointStyle: false,
-        lineTension: 0,
+        // pointStyle: false,
+        lineTension: 0.2,
         pointHitRadius: 30,
       },
     ],
